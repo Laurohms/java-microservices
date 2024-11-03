b3be4af9dbc94eaaed4c4fdf0f88069a1e52e5b9
Laurohms <laurohmsantos@gmail.com>
2024-11-03 10:16:41 -0300

    feat: Api Pedidos

---
+++ pedidos-api/src/main/java/com/ls/pedidosapi/enums/Status.java
@@ -0,0 +1,6 @@
+package com.ls.pedidosapi.enums;
+
+public enum Status {
+    EM_PROCESSAMENTO,
+    PROCESSADO;
+}
---
+++ pedidos-api/src/main/java/com/ls/pedidosapi/service/PedidoService.java
@@ -0,0 +1,28 @@
+package com.ls.pedidosapi.service;
+
+import com.ls.pedidosapi.entity.Pedido;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PedidoService {
+    private final Logger logger = LoggerFactory.getLogger(PedidoService.class);
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public PedidoService (RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public Pedido enfileirarPedido(Pedido pedido) {
+        rabbitTemplate.convertAndSend(exchangeName, "", pedido);
+        logger.info("Pedido enfileirado: {}", pedido.toString());
+        return pedido;
+    }
+}
